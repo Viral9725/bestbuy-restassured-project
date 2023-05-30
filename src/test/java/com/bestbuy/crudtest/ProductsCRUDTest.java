c8ba035fcd86d3435b91ac579dd4bb3cfe68e89c
viral9725 <viral.kakadiya@gmail.com>
2023-05-30 22:12:43 +0100

    Week 17 Viral Kakadiya bestbuy-restassured-project

---
+++ src/test/java/com/bestbuy/crudtest/ProductsCRUDTest.java
@@ -0,0 +1,18 @@
+package com.bestbuy.crudtest;
+
+import com.bestbuy.testbase.TestBase;
+
+public class ProductsCRUDTest extends TestBase {
+
+    static String productId;
+    static String name;
+    static String manufacturer;
+    static String model;
+    static int categories;
+
+
+
+
+
+
+}
---
+++ src/test/java/com/bestbuy/crudtest/StoresCRUDTest.java
@@ -0,0 +1,48 @@
+package com.bestbuy.crudtest;
+
+public class StoresCRUDTest {
+
+
+//    static ValidatableResponse response;
+//
+//    @BeforeClass
+//    public static void inIt() {
+//        RestAssured.baseURI = "http://localhost";
+//        RestAssured.port = 3030;
+//        response = given()
+//                .when()
+//                .get("/stores")
+//                .then().statusCode(200);
+//
+//    }
+//    @Test
+//    public void test004() {
+//        Response response = given()
+//                .contentType(ContentType.JSON)
+//                .when()
+//                .delete("/8922");
+//        response.then().log().all().statusCode(200);
+//    }
+//    StorePojo storePojo = new StorePojo();
+//        storePojo.setName("Tesco");
+//        storePojo.setType("Ashford");
+//        storePojo.setAddress("11 ");
+//        storePojo.setAddress2("cecil Road");
+//        storePojo.setCity("Hounslow");
+//        storePojo.setState("London");
+//        storePojo.setZip("Tw1 3QP");
+//        storePojo.setLat(123345);
+//        storePojo.setLng(12345);
+//        storePojo.setHours("8:00");
+//
+//    Response response = given()
+//            .contentType(ContentType.JSON)
+//            .when()
+//            .body(storePojo)
+//            .post();
+//
+//        response.then().log().all().statusCode(201);
+
+}
+
+
